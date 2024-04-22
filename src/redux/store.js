90ef0061faeca00575a35900defb83e1e67ec495
Mykyta Kudlai <nikiatanik@gmail.com>
2024-04-22 23:29:59 +0300

    upd (fixed error caused by redux persist)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
-import persistStore from "redux-persist/es/persistStore";
+
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistedStore = persistStore(store);
